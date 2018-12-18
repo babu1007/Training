7f08976c8c9ebea73f7e473ee1aae38ff4be0896
Babu <sukurbabu100778@gmail.com>
2018-12-18 09:44:40 +0530

    First commit

---
+++ Decision.py
@@ -0,0 +1,21 @@
+i = 10
+if (i > 15): 
+   print ("10 is less than 15") 
+print ("I am Not in if") 
+j = 20; 
+if (j < 15): 
+    print ("j is smaller than 15") 
+    print ("i'm in if Block") 
+else: 
+    print ("j is greater than 15") 
+    print ("i'm in else Block") 
+print ("i'm not in if and not in else Block") 
+k = 20
+if (k == 10): 
+    print ("k is 10") 
+elif (k == 15): 
+    print ("k is 15") 
+elif (k == 20): 
+    print ("k is 20") 
+else: 
+    print ("k is not present") 
